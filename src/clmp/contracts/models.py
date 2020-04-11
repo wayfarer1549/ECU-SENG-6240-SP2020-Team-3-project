6fa9f89aa4261bafea71f87da59cc8e86a843c4a
rscottjohnson <sjohnson.wvu@gmail.com>
2020-04-11 19:27:36 -0400

    Created the Contracts model and made migrations

--- src/clmp/contracts/models.py
+++ src/clmp/contracts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Contract(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    # add in participant
