8f4ebf20e96fceb65bfaca6c6ed8f4b9f735afc1
rscottjohnson <sjohnson.wvu@gmail.com>
2020-04-11 19:55:56 -0400

    Passing contract data into the contracts template

--- src/clmp/contracts/views.py
+++ src/clmp/contracts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Contract
 
 def contract_list(request):
-    return render(request, 'contracts/contract_list.html')
+    contracts = Contract.objects.all().order_by('date')
+    return render(request, 'contracts/contract_list.html', { 'contracts': contracts })
