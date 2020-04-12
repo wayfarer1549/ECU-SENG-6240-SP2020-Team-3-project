c976112e7fd379aff15c6563193973daa0a8fc10
rscottjohnson <sjohnson.wvu@gmail.com>
2020-04-12 17:53:58 -0400

    Added name to the contract url view path

--- src/clmp/contracts/urls.py
+++ src/clmp/contracts/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.contract_list),
+    path('', views.contract_list, name="list"),
     path('create/', views.contract_create, name="create"),
     re_path('(?P<slug>[\w-]+)/', views.contract_detail, name="detail"),
 ]
