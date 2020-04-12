d9aa533e0800ba9eb456be2405954e298660aac5
rscottjohnson <sjohnson.wvu@gmail.com>
2020-04-12 17:40:58 -0400

    Added slug url and detail view to contracts

--- src/clmp/contracts/urls.py
+++ src/clmp/contracts/urls.py
@@ -1,7 +1,8 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.contract_list),
     path('create/', views.contract_create, name="create"),
+    re_path('(?P<slug>[\w-]+)/', views.contract_detail, name="detail"),
 ]
--- src/clmp/contracts/views.py
+++ src/clmp/contracts/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import Contract
+from django.http import HttpResponse
 
 def contract_list(request):
     contracts = Contract.objects.all().order_by('date')
     return render(request, 'contracts/contract_list.html', { 'contracts': contracts })
+
+def contract_detail(request, slug):
+    return HttpResponse(slug)
 
 # add login required here; redirect if not logged in
 def contract_create(request):
