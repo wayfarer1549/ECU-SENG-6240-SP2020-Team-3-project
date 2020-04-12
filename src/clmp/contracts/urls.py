cb3ee6a33055135612439f0049b28522c16cc3ce
rscottjohnson <sjohnson.wvu@gmail.com>
2020-04-12 17:29:35 -0400

    Added create contract url, view, and template
    Also, removed unused base_generic.html and index.html

--- src/clmp/contracts/urls.py
+++ src/clmp/contracts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.contract_list),
+    path('create/', views.contract_create, name="create"),
 ]
--- src/clmp/contracts/views.py
+++ src/clmp/contracts/views.py
@@ -4,3 +4,7 @@ from .models import Contract
 def contract_list(request):
     contracts = Contract.objects.all().order_by('date')
     return render(request, 'contracts/contract_list.html', { 'contracts': contracts })
+
+# add login required here; redirect if not logged in
+def contract_create(request):
+    return render(request, 'contracts/contracts_create.html')
