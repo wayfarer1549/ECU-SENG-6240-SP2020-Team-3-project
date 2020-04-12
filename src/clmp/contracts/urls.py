277301b5beb9474c58f5e35cbfd7abed0b256d2d
rscottjohnson <sjohnson.wvu@gmail.com>
2020-04-12 18:11:51 -0400

    Adding url data to contract detail view

--- src/clmp/contracts/urls.py
+++ src/clmp/contracts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path, re_path
 from . import views
 
+app_name='contracts'
+
 urlpatterns = [
     path('', views.contract_list, name="list"),
     path('create/', views.contract_create, name="create"),
