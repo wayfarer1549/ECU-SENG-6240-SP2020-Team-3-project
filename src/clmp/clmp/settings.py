82072fa0c3ebfa1f9c4d7ede1709f6c87cb519f6
rscottjohnson <sjohnson.wvu@gmail.com>
2020-04-18 21:19:12 -0400

    Addition for Heroku

--- src/clmp/clmp/settings.py
+++ src/clmp/clmp/settings.py
@@ -129,6 +129,8 @@ LOGIN_REDIRECT_URL = 'homepage'
 
 LOGOUT_REDIRECT_URL='homepage'
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 # Configure Django App for Heroku.
 # import django_heroku
 # django_heroku.settings(locals())
