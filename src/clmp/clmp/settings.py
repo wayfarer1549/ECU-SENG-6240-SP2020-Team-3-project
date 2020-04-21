9ae24e37986ad33f45c2d27df3ddd1e9875708cb
rscottjohnson <sjohnson.wvu@gmail.com>
2020-04-21 15:52:33 -0400

    Update to settings.py

--- src/clmp/clmp/settings.py
+++ src/clmp/clmp/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'assets'),
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'assets'),
 ]
 
 LOGIN_REDIRECT_URL = 'homepage'
