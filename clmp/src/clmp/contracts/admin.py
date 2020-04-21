8ef5c569a25436732ca22a319428908db28f7e71
rscottjohnson <sjohnson.wvu@gmail.com>
2020-04-21 16:16:24 -0400

    Removed subtree push to clmp

--- clmp/src/clmp/contracts/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Contract
-
-# Register your models here.
-admin.site.register(Contract)
--- clmp/src/clmp/contracts/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from . import models
-
-class CreateContract(forms.ModelForm):
-    class Meta:
-        model = models.Contract
-        fields = ['title', 'body', 'slug']
-        
