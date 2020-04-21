1b6e9df41144face1caf4d9b8e960dc543ceb4c4
rscottjohnson <sjohnson.wvu@gmail.com>
2020-04-21 16:06:07 -0400

    Add 'clmp/' from commit '3343d9fb771072b129836b0d81198fbfc8bf18a0'
    git-subtree-dir: clmp
    git-subtree-mainline: 3343d9fb771072b129836b0d81198fbfc8bf18a0
    git-subtree-split: 3343d9fb771072b129836b0d81198fbfc8bf18a0

---
+++ clmp/README.md
@@ -0,0 +1,6 @@
+# ECU-SENG-6240-SP2020-Team-3-project
+Term Project for SENG 6240 Implementation
+<br>spin local server: python manage.py runserver
+<br>whenever you change models.py run:
+<br>python manage.py makemigrations
+<br>python manage.py migrate
---
+++ clmp/src/clmp/contracts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from .models import Contract
+from . import forms
+
+
+def contract_list(request):
+    contracts = Contract.objects.all().order_by('date')
+    return render(request, 'contracts/contract_list.html', { 'contracts': contracts })
+
+def contract_detail(request, slug):
+    # return HttpResponse(slug)
+    contract = Contract.objects.get(slug=slug)
+    if request.method == 'POST' and str(request.user) not in contract.contractStatus :
+        contract.contractStatus = "Approved by " + str(request.user) + ": " + str(timezone.now())
+        contract.save()
+    return render(request, 'contracts/contract_detail.html', { 'contract': contract })
+
+# add login required here; redirect if not logged in
+def contract_create(request):
+    if request.method == 'POST':
+        form = forms.CreateContract(request.POST)
+        if form.is_valid():
+            # save contract to the db
+            # save it as an instance, don't immediately commit the save
+            instance = form.save(commit=False)
+            instance.participant = request.user
+            instance.save()
+            return redirect('contracts:list')
+    else:
+        form = forms.CreateContract()
+    return render(request, 'contracts/contract_create.html', {'form': form })
+
+def contract_approve(request, slug):
+    contract = Contract.objects.get(slug=slug)
+    return render(request, 'contracts/contract_approve.html', {'contract': contract})
+    
