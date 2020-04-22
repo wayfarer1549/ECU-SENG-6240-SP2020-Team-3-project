c626edbaa7efed35121f41855b7613975d739ebd
rscottjohnson <sjohnson.wvu@gmail.com>
2020-04-21 22:56:27 -0400

    Reset of Heroku deployment files / settings

--- clmp/clmp/settings.py
+++ clmp/clmp/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,3 +128,11 @@ STATICFILES_DIRS=[
 LOGIN_REDIRECT_URL = 'homepage'
 
 LOGOUT_REDIRECT_URL='homepage'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
